7416e1e48c909c0995035dda3a900d776cc1c65a
Mattia La Spina <mattiaa95@gmail.com>
2018-08-21 20:20:53 +0100

    correo
    aaa

--- ErickProyectProd/js/main.js
+++ ErickProyectProd/js/main.js
@@ -13,13 +13,14 @@ const sendForm = event => {
 };
 const smtpJS = message => {
     Email.send(
-      "mattiaa95@gmail.com",
+      "bot@jetskitenerifetours.com",
       "info@jetskitenerifetours.com",
       `Nueva reserva - ${message.name} Para: ${message.date}`,
       `${message.name} - ${message.date} - ${message.text} - motos single: ${message.jetskiS} - motos Double: ${message.jetskiD}`,
-       {
-        token: "f10baaec-c38f-457e-921a-632ded53c42c",
-        callback: function done(message) { alert("sent") }
+      {        
+        token: "b397a6ad-0606-4a21-a2c0-062aacdec726"
+        //callback: function done(message) { alert(message) }
+         }
     );
 };
 
